0840c60d95b78e439cfd0eb1db47fda0a1c1bacb
Materuilist <50238654+Materuilist@users.noreply.github.com>
2021-02-25 13:51:03 +0500

    Запрос на получение списка чатов

--- backend/src/controllers/chat.controller.js
+++ backend/src/controllers/chat.controller.js
@@ -1,9 +1,21 @@
 const { Error } = require("../models/shared/error.class");
+const { User } = require("../models/user.model");
 
 const getChats = async (req, res, next) => {
     const { user } = req.body;
 
-    const { chats } = user;
+    const { chats } = await user
+        .populate({
+            path: "chats",
+            select: "_id messages",
+            populate: {
+                path: "messages",
+                select: "content timestamp",
+                options: { sort: { timestamp: -1 } },
+                perDocumentLimit: 1,
+            },
+        })
+        .execPopulate();
 
     res.status(200).json({ chats });
 };
